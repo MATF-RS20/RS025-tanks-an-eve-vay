f4b26d24bd6a601e6ed4acae54fba111c5514849
Stefan <stefan.i@hotmail.rs>
2020-01-04 15:43:12 +0100

    Added some basic functionality to class Object

--- Tanks An Eve Vay/Engine/Object.cpp
+++ Tanks An Eve Vay/Engine/Object.cpp
@@ -1,10 +1,39 @@
 #include "Object.h"
 
-
 Object::Object()
 {
 }
 
+Object::Object(Vector2f position, Vector2f size)
+	:m_Position(position), m_Size(size)
+{
+}
+
+Vector2f Object::GetPosition() const
+{
+	return m_Position;
+}
+
+Vector2f Object::GetSize() const
+{
+	return m_Size;
+}
+
+
+void Object::Move(const Vector2f & dv)
+{
+	m_Position = m_Position + dv;
+}
+
+std::string Object::ToString()
+{
+	return "Position of object: " + m_Position.ToString() + " Size: " + m_Size.ToString();
+}
+
+bool Object::DetectCoalision(const Object * other)
+{
+	return false;
+}
 
 Object::~Object()
 {
--- Tanks An Eve Vay/Engine/Object.h
+++ Tanks An Eve Vay/Engine/Object.h
@@ -6,10 +6,22 @@ class Object
 {
 public:
 	Object();
+	Object(Vector2f position, Vector2f size);
+
+	Object(const Object& o) = default;
+	Object& operator =(const Object& o) = default;
+
+	Vector2f GetPosition() const;
+	Vector2f GetSize() const;
+
+	virtual void Move(const Vector2f& dv);
+	virtual std::string ToString();
+	bool DetectCoalision(const Object* other);
 	~Object();
 
 private:
 	Vector2f m_Position;
 	Vector2f m_Size;
 };
+
 
